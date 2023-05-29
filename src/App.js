aa322fc866733fbca3e28623bf0c45fd3fbd00fb
Kozera <kisaie920@gmail.com>
2023-05-29 17:17:03 +0200

    Deploy5

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import avatar from "./images/avatar.png";
 import logo from "./images/logo.png";
+import united_kingdom from "./images/united-kingdom.png";
+import united_state from "./images/united-states.png";
+
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const JobApplicants = () => {
 
@@ -26,10 +30,9 @@ const JobApplicants = () => {
   const [applicants, setApplicants] = useState([
     {
       id: 1,
-      fullName: 'John Doe',
-      email: 'john@example.com',
+      name_mail: { name: 'John Doe', email: 'john@example.com', image: "" },
       age: 30,
-      phoneNumber: '1234567890',
+      phoneNumber: '+1 (545) 914 53 12',
       position: 'Software Engineer',
       language: 'JavaScript',
       salary: 100000,
@@ -38,10 +41,9 @@ const JobApplicants = () => {
     },
     {
       id: 2,
-      fullName: 'Jane Smith',
-      email: 'jane@example.com',
+      name_mail: { name: 'Jane Smith', email: 'jane@example.com', image: "" },
       age: 25,
-      phoneNumber: '0987654321',
+      phoneNumber: '+1 (566) 966 53 14',
       position: 'UI/UX Designer',
       language: 'HTML, CSS, JavaScript',
       salary: 80000,
@@ -74,6 +76,16 @@ const JobApplicants = () => {
           experience5Plus: checked,
           selectAllExperience: checked ? ['experienceUnder5', 'experience5Plus'] : [],
         }));
+      } else if (name === 'positions') {
+        // Get the updated positions array based on the checkbox status
+        const updatedPositions = checked
+          ? [...filters.positions, value]
+          : filters.positions.filter((position) => position !== value);
+
+        setFilters((prevFilters) => ({
+          ...prevFilters,
+          positions: updatedPositions,
+        }));
       }
     } else {
       setFilters((prevFilters) => ({
@@ -82,6 +94,7 @@ const JobApplicants = () => {
       }));
     }
   };
+
   const handleSearchChange = (event) => { // Handle search field change
     const { value } = event.target;
     setFilters((prevFilters) => ({
@@ -103,12 +116,24 @@ const JobApplicants = () => {
   };
 
   const filteredApplicants = applicants.filter((applicant) => {
-    const { positions, languages, experience, experienceUnder5, experience5Plus, minSalary, maxSalary, search, selectAllExperience } = filters;
-  
+    const {
+      positions,
+      languages,
+      experience,
+      experienceUnder5,
+      experience5Plus,
+      minSalary,
+      maxSalary,
+      search,
+      selectAllExperience,
+    } = filters;
+
     if (positions.length > 0 && !positions.includes(applicant.position)) {
       return false;
     }
-  
+
+
+
     if (
       languages.length > 0 &&
       !languages.some((selectedLanguage) =>
@@ -117,55 +142,86 @@ const JobApplicants = () => {
     ) {
       return false;
     }
-  
+
     if (selectAllExperience.length === 0) {
       if (experience && parseInt(applicant.experience) < 5) {
         return false;
       }
-  
+
       if (experienceUnder5 && parseInt(applicant.experience) >= 5) {
         return false;
       }
-  
+
       if (experience5Plus && parseInt(applicant.experience) < 5) {
         return false;
       }
     }
-  
+
     if (
       minSalary !== '' &&
       parseInt(applicant.salary) < parseInt(minSalary)
     ) {
       return false;
     }
-  
+
     if (
       maxSalary !== '' &&
       parseInt(applicant.salary) > parseInt(maxSalary)
     ) {
       return false;
     }
-  
+
     if (
       search &&
-      !applicant.fullName.toLowerCase().includes(search.toLowerCase()) && // Filter by full name
-      !applicant.email.toLowerCase().includes(search.toLowerCase()) // Filter by email
+      !applicant.name_mail.name.toLowerCase().includes(search.toLowerCase()) && // Filter by full name
+      !applicant.name_mail.email.toLowerCase().includes(search.toLowerCase()) // Filter by email
     ) {
       return false;
     }
-  
+
     return true;
   });
 
 
   const columns = [
-    { field: 'fullName', headerName: 'Full Name', width: 150 },
-    { field: 'email', headerName: 'Email', width: 200 },
+    {
+      field: 'name_mail', headerName: 'Name & Email',
+      width: 250,
+      renderCell: (params) => (
+        <div className='row' style={{ lineHeight: "15px" }}>
+          <div className="col-3">
+            <img src={avatar} className='img-fluid circled' />
+          </div>
+          <div className="col">
+            <span style={{ fontSize: "17px" }}>{params.value.name}</span><br />
+            <small className='text-secondary'>{params.value.email}</small>
+          </div>
+        </div>
+
+      )
+    },
     { field: 'age', headerName: 'Age', width: 120 },
-    { field: 'phoneNumber', headerName: 'Phone Number', width: 150 },
+    { field: 'phoneNumber', headerName: 'Phone Number', width: 200,
+  renderCell: (params) => (
+        <div style={{ lineHeight: "15px" }}>
+          <img src={united_state} className='circled me-2' style={{width:"15px"}}/>
+          <span>{params.value}</span> 
+        </div>
+
+      )
+  },
     { field: 'position', headerName: 'Position', width: 150 },
-    { field: 'language', headerName: 'Language', width: 150 },
-    { field: 'salary', headerName: 'Salary', width: 120 },
+    { field: 'language', headerName: 'Language', width: 200 },
+    {
+      field: 'salary', headerName: 'Salary', width: 230,
+      renderCell: (params) => (
+        <div style={{ lineHeight: "15px" }}>
+          <span style={{ fontSize: "17px" }}>${number_format(params.value, 2)}/year</span><br />
+          <small className='text-secondary'>Average</small>
+        </div>
+
+      )
+    },
     { field: 'experience', headerName: 'Experience', width: 120 },
     { field: 'workType', headerName: 'Work Type', width: 120 }];
 
@@ -196,8 +252,7 @@ const JobApplicants = () => {
   ];
   const rows = filteredApplicants.map((applicant) => ({
     id: applicant.id,
-    fullName: applicant.fullName,
-    email: applicant.email,
+    name_mail: applicant.name_mail,
     age: applicant.age,
     phoneNumber: applicant.phoneNumber,
     position: applicant.position,
@@ -242,13 +297,14 @@ const JobApplicants = () => {
           <Row>
             <Col sm={3}>
               <div className="row mb-3">
-                <div className="col-6">
+                <div className="col-7">
                   <h4>Filters</h4>
                 </div>
-                <div className="col-6">
-                  <Button variant="outline-primary" className="outline-primary w-100" onClick={handleResetFilters}>
+                <div className="col-5">
+                  <Button variant="outline-primary filter-btn" className="outline-primary w-100" onClick={handleResetFilters}>
                     <i class='bx bx-trash me-2'></i>
-                    Clear Filter</Button>
+                    Clear Filter
+                  </Button>
                 </div>
               </div>
 
@@ -262,14 +318,14 @@ const JobApplicants = () => {
                       <div>
 
                         {positions.map((item, index) => (
-                          <FormGroup controlId={"pos-" + index}>
+                          <FormGroup controlId={`pos-${index}`} key={index}>
                             <Form.Check
-                              controlId="formBasicCheckbox"
                               type="checkbox"
                               label={item.name}
                               name="positions"
                               value={item.name}
                               onChange={handleFilterChange}
+                              checked={filters.positions.includes(item.name)}
                             />
                           </FormGroup>
                         ))}
@@ -367,8 +423,24 @@ const JobApplicants = () => {
               </Form>
             </Col>
             <Col sm={9}>
-              <h4>Candidates</h4>
-              <div style={{ height: 400, width: '100%' }}>
+              <div className="row my-2 search">
+                <div className="col-md-6">
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text id="Search">
+                      <i class='bx bx-search'></i>
+                    </InputGroup.Text>
+                    <Form.Control
+                      placeholder="Search any user.."
+                      aria-label="search"
+                      aria-describedby="Search"
+                      onChange={handleSearchChange}
+                    />
+                  </InputGroup>
+                </div>
+              </div>
+
+
+              <div style={{ height: 900, width: '100%' }}>
                 {/* Add GridToolbar component to display search field */}
                 <DataGrid
                   rows={rows}
@@ -376,6 +448,7 @@ const JobApplicants = () => {
                   pageSize={5}
                   checkboxSelection
                   disableSelectionOnClick
+                  getRowHeight={() => 'auto'}
                   components={{
                     Toolbar: GridToolbar, // Use GridToolbar component
                   }}
@@ -398,5 +471,34 @@ const JobApplicants = () => {
     </div>
   );
 };
+function number_format(number, decimals, dec_point, thousands_point) {
+
+  if (number == null || !isFinite(number)) {
+    throw new TypeError("number is not valid");
+  }
+
+  if (!decimals) {
+    var len = number.toString().split('.').length;
+    decimals = len > 1 ? len : 0;
+  }
+
+  if (!dec_point) {
+    dec_point = '.';
+  }
+
+  if (!thousands_point) {
+    thousands_point = ',';
+  }
+
+  number = parseFloat(number).toFixed(decimals);
+
+  number = number.replace(".", dec_point);
+
+  var splitNum = number.split(dec_point);
+  splitNum[0] = splitNum[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_point);
+  number = splitNum.join(dec_point);
+
+  return number;
+}
 
 export default JobApplicants;
